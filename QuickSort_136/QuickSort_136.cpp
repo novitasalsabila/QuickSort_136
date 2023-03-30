188401034b7e7fba27afb3332af62539139966b2
novitasalsabila <novnovv10@gmail.com>
2023-03-30 15:13:49 +0700

    Menggunakan prosedure inputan dan menggunakan looping while

--- QuickSort_136/QuickSort_136.cpp
+++ QuickSort_136/QuickSort_136.cpp
@@ -5,3 +5,18 @@ int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang elemen pada array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else;
+		cout << "\nMaksimum panjang pada array adalah 20"
+	}
+
+
+}
