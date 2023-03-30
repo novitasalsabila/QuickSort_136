f89e73bd7402a1730f78a20dfedd03aca2212465
novitasalsabila <novnovv10@gmail.com>
2023-03-30 15:21:26 +0700

    Menggunakan perulangan for

--- QuickSort_136/QuickSort_136.cpp
+++ QuickSort_136/QuickSort_136.cpp
@@ -15,8 +15,15 @@ void input() {
 		if (n <= 20)
 			break;
 		else;
-		cout << "\nMaksimum panjang pada array adalah 20"
+		cout << "\nMaksimum panjang pada array adalah 20" << endl;
 	}
+	cout << "\n===================" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n-------------------" << endl;
 
-
+	for (int i = 0;i < n;i++)
+	{
+		cout << "<" << (i + i) << "<";
+		cin >> arr[i];
+	}
 }
